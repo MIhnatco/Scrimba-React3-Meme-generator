65c90f1b8d6db3272d1fdfd7bf5b78be968464ad
Ihliana <Ihnatco@protonmail.com>
2023-07-30 18:38:07 +0200

    get random meme
    memesData file with data and looking for random image url

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,31 @@
 import React from "react"
 
+import memesData from '../memesData'
+
+   /**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     * 
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+     */
+
 
 function Meme(){
+
+    function getImage(){
+        let url = memesData.data.memes[Math.floor( Math.random() * memesData.data.memes.length )].url;
+
+        console.log(url)
+
+    }
+
+
+
+
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input  type="text" 
                         placeholder="Top text"
                         className="form--input"
@@ -15,8 +36,8 @@ function Meme(){
                         placeholder="Bottom text"
                         />
 
-                <button className="form--button">Get a new meme image 🖼</button>
-            </form>
+                <button className="form--button" onClick={getImage}>Get a new meme image 🖼</button>
+            </div>
         </main>
 
     )
