37ca2df29bd3882b756daa6bc820eeee6d818acd
Ihliana <Ihnatco@protonmail.com>
2023-07-30 18:09:33 +0200

    Header component
    creating header part

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Troll from "../images/troll-face.png"
+
+function Header(){
+    return(
+        <header className='header'>
+            <img className='header--image' src={Troll} alt='' />
+            <h2 className='header--title'>Meme generator</h2>
+            <h4 className='header--project'>React Course - Project 3</h4>
+        </header>
+    )
+}
+
+
+export default Header
