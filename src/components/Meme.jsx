fc60427a42c1862ea0961cd742357bc0913aac2f
Ihliana <Ihnatco@protonmail.com>
2023-08-01 12:33:32 +0200

    refactor state
    creating object in useState

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,29 +1,41 @@
 import React from "react"
 
 import memesData from '../memesData'
-    /**
-     * Challenge: Save the random meme URL in state
-     * - Create new state called `memeImage` with an
-     *   empty string as default
-     * - When the getMemeImage function is called, update
-     *   the `memeImage` state to be the random chosen
-     *   image URL
-     * - Below the div.form, add an <img /> and set the
-     *   src to the new `memeImage` state you created
-     */
 
+ /**
+     * Challenge: Update our state to save the meme-related
+     * data as an object called `meme`. It should have the
+     * following 3 properties:
+     * topText, bottomText, randomImage.
+     * 
+     * The 2 text states can default to empty strings for now,
+     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * 
+     * Next, create a new state variable called `allMemeImages`
+     * which will default to `memesData`, which we imported above
+     * 
+     * Lastly, update the `getMemeImage` function and the markup 
+     * to reflect our newly reformed state object and array in the
+     * correct way.
+     */
 
 function Meme(){
 
     const [memeImage, setMemeImage] = React.useState("")
 
+    const [meme, setMeme] = React.useState({
+        topText: "", 
+        bottomText: "", 
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
 
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage(){
-        let url = memesData.data.memes[Math.floor( Math.random() * memesData.data.memes.length )].url;
-
-        setMemeImage(url)
-
+        let url = allMemeImages.data.memes[Math.floor( Math.random() * allMemeImages.data.memes.length )].url;
+        setMeme(prevMeme => ({                ...prevMeme, 
+                randomImage: url
+            }))
     }
 
 
@@ -43,7 +55,7 @@ function Meme(){
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
 
-            <img src={memeImage} alt="" className="meme--image"/>
+            <img src={meme.randomImage} alt="" className="meme--image"/>
         </main>
 
     )
