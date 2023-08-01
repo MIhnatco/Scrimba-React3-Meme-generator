376f3701d6963bf60be69f0a77cce073e58492c6
Ihliana <Ihnatco@protonmail.com>
2023-08-01 13:22:44 +0200

    Adding comments

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,35 +2,20 @@ import React from "react"
 
 import memesData from '../memesData'
 
- /**
-     * Challenge: Update our state to save the meme-related
-     * data as an object called `meme`. It should have the
-     * following 3 properties:
-     * topText, bottomText, randomImage.
-     * 
-     * The 2 text states can default to empty strings for now,
-     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
-     * 
-     * Next, create a new state variable called `allMemeImages`
-     * which will default to `memesData`, which we imported above
-     * 
-     * Lastly, update the `getMemeImage` function and the markup 
-     * to reflect our newly reformed state object and array in the
-     * correct way.
-     */
-
+//React component that represents meme generator
 function Meme(){
-
-    const [memeImage, setMemeImage] = React.useState("")
-
+    //state variable holds the topText, bottomText and randomImage
     const [meme, setMeme] = React.useState({
         topText: "", 
         bottomText: "", 
-        randomImage: "http://i.imgflip.com/1bij.jpg"
+        randomImage: "http://i.imgflip.com/1bij.jpg"   //default random meme image
     })
 
+    //state variable stores an array of meme data fetched from memesData
     const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
+
+    //selects a random meme image URL from 'allMemeImages' array
     function getMemeImage(){
         let url = allMemeImages.data.memes[Math.floor( Math.random() * allMemeImages.data.memes.length )].url;
         setMeme(prevMeme => ({                ...prevMeme, 
@@ -42,19 +27,23 @@ function Meme(){
     return(
         <main>
             <div className="form">
+                {/*Input field for entering the topText of the meme*/}
                 <input  type="text" 
                         placeholder="Top text"
                         className="form--input"
                         />
 
+                {/*Input field for entering the bottomText of the meme*/}
                 <input type="text"
                         className="form--input" 
                         placeholder="Bottom text"
                         />
 
+                {/*button triggers the 'getMemeImage' function to fetch an image*/}
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
 
+            {/*the selected meme image is displayed here*/}
             <img src={meme.randomImage} alt="" className="meme--image"/>
         </main>
 
